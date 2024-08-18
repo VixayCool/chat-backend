77fff0122eafe8cc7d29b4513fb5dde898a39f4e
VIXAY VONGPUNYA <vongpunyavixay@gmail.com>
2024-08-18 11:50:37 +0900

    updated-cors

--- config/sanctum.php
+++ config/sanctum.php
@@ -17,7 +17,7 @@ return [
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s',
-        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
+        'localhost,https://white-moss-0f8134a1e.5.azurestaticapps.net,127.0.0.1,127.0.0.1:8000,::1',
         Sanctum::currentApplicationUrlWithPort()
     ))),
 
