1034c928dd09073379084cad55b399f74a9f84b6
VIXAY VONGPUNYA <vongpunyavixay@gmail.com>
2024-08-26 10:13:28 +0900

    updated-broadcasting-route

--- routes/api.php
+++ routes/api.php
@@ -1,4 +1,5 @@
 <?php
+use Illuminate\Support\Facades\Broadcast;
 use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\Api\FriendshipController;
 use App\Http\Controllers\Api\MessageController;
@@ -48,12 +49,9 @@ Route::delete('/group/{group_id}/member/delete/{friend_id}', [GroupController::c
 Route::delete('/group/leave/{id}', [GroupController::class, 'leaveGroup'])->middleware('auth:sanctum');
 
 Route::middleware('auth:sanctum')->post('/broadcasting/auth', function (Request $request) {
-    return true; // Change later
-}); 
-//debug
-Route::get('/example', function (Request $request) {
-    return response()->json(['message' => 'This is an example route']);
+    return Broadcast::auth($request);
 });
+
 Broadcast::routes(['middleware' => ['auth:sanctum']]);
 
 require base_path('routes/channels.php');
