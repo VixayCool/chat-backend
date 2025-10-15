2251fa1fee23f1808edb9b4df1e72f9e81aff21b
VIXAY VONGPUNYA <vongpunyavixay@gmail.com>
2025-10-15 13:00:57 +0900

    add ssl cert

--- config/database.php
+++ config/database.php
@@ -55,8 +55,7 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => '/home/site/wwwroot/certs/BaltimoreCyberTrustRoot.crt.pem',
-                // env('MYSQL_ATTR_SSL_CA'),
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
         ],
 
