6dd3ccc1a4b9a6bee2b43b9d43f306dd54debfd6
VIXAY VONGPUNYA <vongpunyavixay@gmail.com>
2025-10-15 09:49:25 +0900

    cache clear

--- config/sanctum.php
+++ config/sanctum.php
@@ -8,7 +8,7 @@ return [
     |--------------------------------------------------------------------------
     | Stateful Domains
     |--------------------------------------------------------------------------
-    |
+    |   
     | Requests from the following domains / hosts will receive stateful API
     | authentication cookies. Typically, these should include your local
     | and production domains which access your API via a frontend SPA.
@@ -17,7 +17,7 @@ return [
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s',
-        ' victorious-stone-0a123d900.3.azurestaticapps.net,localhost, 127.0.0.1,127.0.0.1:8000,::1',
+        'victorious-stone-0a123d900.3.azurestaticapps.net,localhost, 127.0.0.1,127.0.0.1:8000,::1',
         Sanctum::currentApplicationUrlWithPort()
     ))),
 
